6d83dbcc1df910ce0479217aad3e1296f8f43231
remyCESI <remy.vancamp@viacesi.fr>
2021-02-07 20:57:40 +0100

    section actu à reflechir

--- Index.js
+++ Index.js
@@ -1,21 +1,31 @@
+$(window).on('load', function() {
+
+    $("#imgload").fadeOut("2000");
+})
 $(document).ready(function() {
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
+        nav: false,
+        dots: true,
+        pagination: false,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
-                nav: true
+                nav: false
             },
             600: {
-                items: 3,
+                items: 2,
                 nav: false
             },
             1000: {
-                items: 5,
-                nav: true,
-                loop: false
+                items: 2,
+
+
             }
         }
     })
