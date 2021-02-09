57a92a880dc761552db4b03be23b4c8dbc891a44
remyCESI <remy.vancamp@viacesi.fr>
2021-02-09 17:40:04 +0100

    ML + Animation JS

--- Index.js
+++ Index.js
@@ -1,8 +1,37 @@
+const Sr = ScrollReveal({
+    duration: 2000,
+    reset: true,
+    mobile: true
+})
 $(window).on('load', function() {
 
     $("#imgload").fadeOut("2000");
 })
 $(document).ready(function() {
+    Sr.reveal('.logo-container')
+    Sr.reveal('.introduction', {
+        delay: 200
+    })
+    Sr.reveal('.cover', {
+        delay: 200
+    })
+    Sr.reveal('.ActionPrez', {
+        delay: 500
+    })
+
+    Sr.reveal('H1', {
+        duration: 500
+    })
+
+    Sr.reveal('.Actu', {
+        duration: 500
+    }, 50)
+
+
+
+
+
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
